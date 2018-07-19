4511b2503f64688e806c985c186f4c92bbe3ec43
Kristoffer <kba_93@live.se>
2018-07-19 19:16:18 +0900

    add threading

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from IoTClient import IoTClient
+import threading
 
 if __name__ == '__main__':
     client = IoTClient()
+
+    thread = threading.Thread(target=client.run, args=())
+    thread.daemon = True  # Daemonize thread
+    thread.start()  # Start the execution
