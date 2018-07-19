b89b96a8a31df8c47ad395f04fbfaf7564ca8a58
Kristoffer <kba_93@live.se>
2018-07-19 19:19:21 +0900

    import update

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from IoTClient import IoTClient
+from IoTClient.IoTClient import IoTClient
 import threading
 
 if __name__ == '__main__':
     client = IoTClient()
 
-    thread = threading.Thread(target=client.run, args=())
-    thread.daemon = True  # Daemonize thread
-    thread.start()  # Start the execution
+    client.run()
+
+    # thread = threading.Thread(target=client.run, args=())
+    # thread.daemon = True  # Daemonize thread
+    # thread.start()  # Start the execution
