e0f45948e8c8fa7cd5f6bd3c0e001e32ead83f1b
Kristoffer <kba_93@live.se>
2018-07-26 14:13:39 +0900

    Threading iot client

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from IoTClient.IoTClient import IoTClient
 import threading
+import time
 
 if __name__ == '__main__':
     client = IoTClient()
 
-    client.run()
+    # client.run()
 
-    # thread = threading.Thread(target=client.run, args=())
-    # thread.daemon = True  # Daemonize thread
-    # thread.start()  # Start the execution
+    thread = threading.Thread(target=client.run, args=())
+    thread.daemon = True  # Daemonize thread
+    thread.start()  # Start the execution
+
+    while True:
+        time.sleep(1)
